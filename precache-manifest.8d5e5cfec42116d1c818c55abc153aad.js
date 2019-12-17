bad23ebf852199b432372987b3ae7535c8f1152b
arijitiiest <1999arijitchoudhury@gmail.com>
2019-12-17 12:48:29 +0530

    Updates

---
+++ precache-manifest.8d5e5cfec42116d1c818c55abc153aad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88aa54b78438a5b1147d10dd53f4514",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fa2927d0d25bb9b8546f",
+    "url": "/portfolio/static/css/main.843b24cf.chunk.css"
+  },
+  {
+    "revision": "62dd2053f5f0730739f6",
+    "url": "/portfolio/static/js/2.4ae3ccfe.chunk.js"
+  },
+  {
+    "revision": "fa2927d0d25bb9b8546f",
+    "url": "/portfolio/static/js/main.fdd31b17.chunk.js"
+  },
+  {
+    "revision": "364100cbeac5828147c3",
+    "url": "/portfolio/static/js/runtime-main.ca3e13b3.js"
+  },
+  {
+    "revision": "54e4e66885c5c1f524d836762b9f2f84",
+    "url": "/portfolio/static/media/me.54e4e668.jpg"
+  }
+]);
