56f20dc375011e0ef54222fc923469a122dfe74b
arijitiiest <1999arijitchoudhury@gmail.com>
2019-12-17 12:56:08 +0530

    Second

--- src/App/Root.js
+++ src/App/Root.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, HashRouter as Router, Switch } from 'react-router-dom';
 
 import './Root.css';
 import Home from './Views/Home/Home';
