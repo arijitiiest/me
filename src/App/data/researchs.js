29c81bb6bf62abec60c4c81e55a759aab24c309c
arijitiiest <1999arijitchoudhury@gmail.com>
2020-09-01 17:19:57 +0530

    one item added

--- src/App/data/researchs.js
+++ src/App/data/researchs.js
@@ -4,13 +4,24 @@ const researchs = [
     {
         title: 'BIOCHIP TESTING',
         desc: "Iterative Parallel Test to Detect and Diagnose of Multiple Defects for Digital Microfluidic Biochip",
-        gitRepo: 'https://github.com/arijitiiest/Biochip-Testing',
+        gitRepo: 'https://github.com/arijitiiest/Biochip-Testing-Iterative-Parallel-Test',
         date: 'DECEMBER, 2019',
         image: biochip,
         published_in: '2019 IEEE 28th Asian Test Symposium (ATS)',
         DOI: '10.1109/ATS47505.2019.00027',
         link: 'https://ieeexplore.ieee.org/document/8949416',
         ieee_added: '06 January 2020'
+    },
+    {
+        title: 'BIOCHIP TESTING',
+        desc: "Efficient Fault Detection and Diagnosis of Digital Microfluidic Biochip Using Multiple Electrodes Actuation",
+        gitRepo: 'https://github.com/arijitiiest/Biochip-Testing-Multiple-Electrodes-Actuation',
+        date: 'JULY, 2020',
+        image: biochip,
+        published_in: '2020 IEEE International Test Conference India',
+        DOI: '10.1109/ITCIndia49857.2020.9171793',
+        link: 'https://ieeexplore.ieee.org/document/9171793',
+        ieee_added: '20 August 2020'
     }
 ];
 
